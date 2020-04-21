b261dc50cc5debcee8ec6b78179bef1b628c2010
Ericson Valentim <59890033+EricsonValentim@users.noreply.github.com>
2020-04-20 23:25:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const palavraChavesRoutes = require("./api/routes/palavra_chave");
 const externo = require("./api/routes/externo");
 const forms = require("./api/routes/forms");
 const forum = require("./api/routes/forum");
+const noticiasRoutes = require("./api/routes/noticias");
 
 const senha_banco = "portifolio-corona-api";
 
@@ -53,6 +54,7 @@ app.use("/cidade", cidadesRoutes);
 app.use("/externo", externo);
 app.use("/forms", forms);
 app.use("/forum", forum);
+app.use("/noticias", noticiasRoutes);
 
 app.use((req, res, next) => {
   const error = new Error("Não encontrado");
