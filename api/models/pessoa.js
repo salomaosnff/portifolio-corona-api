59eac4cce19cca16ef3bb568cf695a90291f1897
renatoalves0 <renatoalves97.123@gmail.com>
2020-04-22 10:52:18 -0300

    Add campo "administrador" em Pessoa

--- api/models/pessoa.js
+++ api/models/pessoa.js
@@ -2,18 +2,17 @@ const mongoose = require('mongoose')
 const pessoa = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     nome: { type: String, required: true },
-    email: { type: String, required: true }, // nome_usuario é o email
+    email: { type: String, required: true },
     telefone: { type: String, required: true },
     cpf: { type: String },
     cnpj: { type: String },
     tipo: { type: String, required: true },
-    // nome_usuario: { type: String, required: true },
     senha: { type: String, required: true },
     colaborador: { type: Boolean },
     investidor: { type: Boolean },
     cliente: { type: Boolean },
-    whatsapp: { type: String }
-    //endereco: { type: mongoose.Schema.Types.ObjectId, ref: 'Endereco', required: true },
+    whatsapp: { type: String },
+    administrador: { type: Boolean }
 })
 
 module.exports = mongoose.model('Pessoa', pessoa)
