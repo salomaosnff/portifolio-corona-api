b63659481c3c13382a7651ec1612fd913f57ff27
renatoalves0 <renatoalves97.123@gmail.com>
2020-05-15 11:50:35 -0300

    Comentando códigos

--- api/routes/cidade.js
+++ api/routes/cidade.js
@@ -2,33 +2,58 @@ const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
 const Cidade = require("../models/cidade");
-const Estado = require("../models/estado");
+// const Estado = require("../models/estado");
 
-router.post("/cadastrarCidades", (req, res) => {
-  req.body.estados.forEach((element) => {
-    Estado.find({ nome: element.nome }, function (err, arr) {
-      var estadoX;
-      arr.forEach((estado) => {
-        estadoX = estado._id;
-      });
-      element.cidades.forEach((cid) => {
-        const cidade = new Cidade({
-          _id: new mongoose.Types.ObjectId(),
-          estado: estadoX,
-          nome: cid,
-        });
-        cidade
-          .save()
-          .then(() => {
-            res
-              .status(201)
-              .json({ message: "Salvo com sucesso!", _id: cidade._id });
-          })
-          .catch((err) => res.status(500).json({ error: err }));
-      });
-    });
-  });
-});
+// router.post("/cadastrarCidades", (req, res) => {
+//   req.body.estados.forEach((element) => {
+//     Estado.find({ nome: element.nome }, function (err, arr) {
+//       var estadoX;
+//       arr.forEach((estado) => {
+//         estadoX = estado._id;
+//       });
+//       element.cidades.forEach((cid) => {
+//         const cidade = new Cidade({
+//           _id: new mongoose.Types.ObjectId(),
+//           estado: estadoX,
+//           nome: cid,
+//         });
+//         cidade
+//           .save()
+//           .then(() => {
+//             res
+//               .status(201)
+//               .json({ message: "Salvo com sucesso!", _id: cidade._id });
+//           })
+//           .catch((err) => res.status(500).json({ error: err }));
+//       });
+//     });
+//   });
+// });
+
+// Métopo para excluir as duplicatas
+// router.get("/", (req, res) => {
+//   Cidade.find()
+//     .populate("estado")
+//     .exec()
+//     .then(async x => {
+//       let cidades = x
+//       let duplicatas = []
+//       await cidades.forEach(async (cidade, i) => {
+//         for (let index = i + 1; index < cidades.length; index++) {
+//           if (await cidade.nome == cidades[index].nome) {
+//             if (await cidade.estado._id == cidades[index].estado._id) {
+//               console.log('Achei um')
+//               await duplicatas.push(cidades[index])
+//               await Cidade.remove({ _id: cidades[index]._id }).exec()
+//               await cidades.splice(index, 1)
+//             }
+//           }
+//         }
+//       })
+//       res.status(200).json(cidades)
+//     })
+//     .catch((err) => res.status(500).json({ error: err }));
+// });
 
 router.get("/", (req, res) => {
   Cidade.find()
